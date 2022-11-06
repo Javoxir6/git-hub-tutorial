fb345db13bdf508bb5f8b8206198721f71caae14
Javoxir <example@gmail.com>
2022-11-06 18:19:58 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+ "use strict";
+
+// const stat = {
+//     age : 20,
+//     jacket : "black",
+//     isLong : true,
+// };
+
+// console.log(stat.age);
+
+// const colors = ["red", "yelolow", "grey"];
+// console.log(colors[1]);
+
+// const place = confirm("are you here?");
+// console.log(place);
+
+// let currentPlace = prompt("Where are you?", "andijan");
+// console.log(typeof currentPlace);
+
+// const channel = prompt("qui est ton youtuber prefere?");
+// console.log(`https://youtube.com/${channel}`);
+
+// let user = "Javoxir";
+// let job = "Prof";
+
+// console.log(`User name is ${user} , he is ${job}`)
+
+// const isAge = true;
+// const isClose = false;
+// const isChecked = false;
+
+// console.log(isAge || isChecked)
